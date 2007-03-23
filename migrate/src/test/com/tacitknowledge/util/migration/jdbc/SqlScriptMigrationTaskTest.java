d107985cdcbd494da3dc91c996f74c72c25cf0ad
mikehardy <mikehardy>
2007-03-23 13:19:24 +0000

    checkstyle alteration

--- migrate/src/test/com/tacitknowledge/util/migration/jdbc/SqlScriptMigrationTaskTest.java
+++ migrate/src/test/com/tacitknowledge/util/migration/jdbc/SqlScriptMigrationTaskTest.java
@@ -17,8 +17,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import org.easymock.MockControl;
-
 import com.mockrunner.jdbc.JDBCTestCaseAdapter;
 import com.mockrunner.mock.jdbc.MockConnection;
 import com.tacitknowledge.util.migration.MigrationException;
