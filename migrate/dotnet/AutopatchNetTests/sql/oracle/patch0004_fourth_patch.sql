f71515dfbadcdc9d7c92699f02db4db4aeb4bbf9
vgangantk <vgangantk>
2007-03-23 20:13:31 +0000

    Added more test SQL scripts

---
+++ migrate/dotnet/AutopatchNetTests/sql/oracle/patch0004_fourth_patch.sql
@@ -0,0 +1,13 @@
+
+   insert into user_role_assoc (user_role_id, application_user_id, role_code, project_id) 
+			values (nextval('role_id_seq'),2, 'SYSA', 3);    
+
+// Testing
+insert into user_role_assoc (user_role_id, application_user_id, role_code, project_id) 
+			values (nextval('role_id_seq'),3, 'SYSA', 3);
+
+   -- This is a comment
+insert into user_role_assoc (user_role_id, application_user_id, role_code, project_id) 
+			values (nextval('role_--id_seq;'),4, 'SYSA', 3);
+			
+	
