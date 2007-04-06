45a54a2e9001d1897502699b067da6716c6fe9c9
asoto <asoto>
2007-04-06 20:42:31 +0000

    throw a MojoFailureException when MigrationException occurs so a maven build will fail when patching fails

--- autopatch-maven-plugin/src/main/java/com/tacitknowledge/autopatch/maven/DistributedPatchExecutionMojo.java
+++ autopatch-maven-plugin/src/main/java/com/tacitknowledge/autopatch/maven/DistributedPatchExecutionMojo.java
@@ -43,6 +43,7 @@ public class DistributedPatchExecutionMojo extends AbstractAutoPatchMojo {
         catch (Exception e) 
         {
             getLog().error(e);
+            throw new MojoFailureException(e.getMessage());
         }
     }
 
