b9a9b84fb0970218099bdf058d10384e198ba798
asoto <asoto>
2007-10-20 00:22:06 +0000

    fixed license

--- migrate/src/optional/spring/com/tacitknowledge/util/migration/jdbc/spring/SpringAutoPatchService.java
+++ migrate/src/optional/spring/com/tacitknowledge/util/migration/jdbc/spring/SpringAutoPatchService.java
@@ -1,7 +1,14 @@
-/*
- * Copyright (c) Shoreline Associates X, LLC. 444 High Street, Suite 400
- * Palo Alto, CA 94301.  All Rights Reserved. This software is the confidential
- * and proprietary information of Shoreline Associates X, LLC.
+/* Copyright 2007 Tacit Knowledge LLC
+ * 
+ * Licensed under the Tacit Knowledge Open License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.tacitknowledge.com/licenses-1.0.
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.tacitknowledge.util.migration.jdbc.spring;
