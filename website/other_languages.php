b7742ddf365bbd32c5505ac281ad0ac540d86bc2
mikehardy <mikehardy>
2007-11-01 20:51:23 +0000

    upgrading PHP's described support to indicate that it's pretty good now

--- website/other_languages.php
+++ website/other_languages.php
@@ -9,9 +9,13 @@
 
 <p>As such, we have implemented this idea (to a larger or smaller degree) in other languages as well.
 
+<p>We have pretty good / used in production implementations in:
+<ul>
+<li><a href="http://autopatch.cvs.sourceforge.net/autopatch/autopatch/migrate/php/">PHP</a></li>
+</ul>
+
 <p>We have basic implementations in:
 <ul>
-<li><a href="http://autopatch.cvs.sourceforge.net/autopatch/autopatch/migrate/php/">PHP</a>
 <li><a href="http://autopatch.cvs.sourceforge.net/autopatch/autopatch/migrate/ruby/">Ruby</a> (it's different than stock Migrations - it works on Windows/SQLServer)
 <li><a href="http://autopatch.cvs.sourceforge.net/autopatch/autopatch/migrate/dotnet/">.NET</a>.
 </ul>
