1cbbb9202d1077d86f7254de162737894fedf8c9
apeshimam <apeshimam>
2008-05-13 14:10:29 +0000

    added up method as a no op in case some legacy tasks extended this class rather than implemented MigrationTask. Also performed code clean up.

--- migrate/src/java/com/tacitknowledge/util/migration/MigrationTaskSupport.java
+++ migrate/src/java/com/tacitknowledge/util/migration/MigrationTaskSupport.java
@@ -18,18 +18,18 @@ import javax.naming.OperationNotSupportedException;
 /**
  * Convenience base class for migration tasks.
  * 
- * @author  Scott Askew (scott@tacitknowledge.com)
+ * @author Scott Askew (scott@tacitknowledge.com)
  * @author Artie Pesh-Imam (apeshimam@tacitknowledge.com)
  */
 public abstract class MigrationTaskSupport implements RollbackableMigrationTask
 {
-	protected boolean isRollbackSupported = false;
-	
+    protected boolean isRollbackSupported = false;
+
     /**
      * The name of this migration task
      */
     private String name;
-    
+
     /**
      * The relative order in which this test should run
      */
@@ -38,70 +38,91 @@ public abstract class MigrationTaskSupport implements RollbackableMigrationTask
     /** {@inheritDoc} */
     public String getName()
     {
-        return name;
+	return name;
     }
-    
+
     /**
      * Sets the name of this migration task.
      * 
-     * @param name the name of this migration task
+     * @param name
+     *                the name of this migration task
      */
     public void setName(String name)
     {
-        this.name = name;
+	this.name = name;
     }
 
     /** {@inheritDoc} */
     public Integer getLevel()
     {
-        return level;
+	return level;
     }
-    
+
     /**
      * Sets the relative order in which this test should run
      * 
-     * @param lvl the relative order in which this test should run
+     * @param lvl
+     *                the relative order in which this test should run
      */
     public void setLevel(Integer lvl)
     {
-        this.level = lvl;
+	this.level = lvl;
     }
 
     /** {@inheritDoc} */
     public int compareTo(Object o)
     {
-        MigrationTask task = (MigrationTask) o;
-        if (task.getLevel() == null)
-        {
-            return 1;
-        }
-        return getLevel().compareTo(task.getLevel());
+	MigrationTask task = (MigrationTask) o;
+	if (task.getLevel() == null)
+	{
+	    return 1;
+	}
+	return getLevel().compareTo(task.getLevel());
     }
 
     /**
      * By default, this method is not supported.
      */
-	public void down(MigrationContext context) throws MigrationException {
-		throw new UnsupportedOperationException("This method is not supported by this task.");	
-	}
+    public void down(MigrationContext context) throws MigrationException
+    {
+	throw new UnsupportedOperationException(
+		"This method is not supported by this task.");
+    }
 
-	/**
-	 * @return a boolean indicating if rollback is supported
-	 */
-	public boolean isRollbackSupported() {
-		return isRollbackSupported;
-	}
-	
-	/**
-	 * Sets the isRollbackSupported attribute
-	 * @param isRollbackSupported
-	 */
-	public void setRollbackSupported(boolean isRollbackSupported)
-	{
-		this.isRollbackSupported = isRollbackSupported;
-	}
-	
-	public void up(MigrationContext context) throws MigrationException {
-		migrate(context);
-	}
+    /**
+     * @return a boolean indicating if rollback is supported
+     */
+    public boolean isRollbackSupported()
+    {
+	return isRollbackSupported;
+    }
+
+    /**
+     * Sets the isRollbackSupported attribute
+     * 
+     * @param isRollbackSupported
+     */
+    public void setRollbackSupported(boolean isRollbackSupported)
+    {
+	this.isRollbackSupported = isRollbackSupported;
+    }
+
+    /**
+     * By default, this method delegates to the up method.
+     */
+    public void migrate(MigrationContext context) throws MigrationException
+    {
+	up(context);
+    }
+
+    /**
+     * By default, this method is a no-op. If a legacy task extends
+     * MigrationTaskSupport but does not implement up, it would cause a
+     * compilation error. This no-op method resolves that issue.
+     */
+    public void up(MigrationContext context) throws MigrationException
+    {
+	// no op
+    }
+
 }
