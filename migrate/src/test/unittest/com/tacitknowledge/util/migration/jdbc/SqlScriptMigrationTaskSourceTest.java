e2f5a1ffefe9c57f6fbf7a64a9bcaef6a85583e2
apeshimam <apeshimam>
2008-05-14 21:39:09 +0000

    updated test case to test both up and down scripts

--- migrate/src/test/unittest/com/tacitknowledge/util/migration/jdbc/SqlScriptMigrationTaskSourceTest.java
+++ migrate/src/test/unittest/com/tacitknowledge/util/migration/jdbc/SqlScriptMigrationTaskSourceTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 import junit.framework.TestCase;
 
 import com.tacitknowledge.util.migration.MigrationException;
+import com.tacitknowledge.util.migration.MigrationTaskSupport;
 
 /**
  * Exercise the SqlScriptMigrationTaskSource
@@ -43,5 +44,28 @@ public class SqlScriptMigrationTaskSourceTest extends TestCase
         
         // There are 3 scripts in our package of scripts, make sure they are all here
         assertEquals(3, tasks.size());
+    }
+    
+    /**
+     * Test that a Migration which does not have a rollback script correctly 
+     * returns false for isRollbackSupported.
+     */
+    public void testNonRollbackableScript() {
+	 SqlScriptMigrationTaskSource source = new SqlScriptMigrationTaskSource();
+	 List tasks = null;
+	 MigrationTaskSupport task = null;
+	 try 
+	 {
+	     tasks = source.getMigrationTasks(this.getClass().getPackage().getName() + ".test");
+	     task = (SqlScriptMigrationTask) tasks.get(0);
+	     assertTrue(task.isRollbackSupported());
+	     task = (SqlScriptMigrationTask) tasks.get(1);
+	     assertFalse(task.isRollbackSupported());
+	     
+	 }
+	 catch (MigrationException me)
+	 {
+	     fail("There shouldn't have been a problem loading the tasks");
+	 }
     }
 }
