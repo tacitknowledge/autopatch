9c5e3e3dfa3531d26f2c968b07c3885a24cb7b1a
apeshimam <apeshimam>
2008-05-14 21:34:43 +0000

    refactored quite a bit to shorted methods and also added logic to read both up and down scripts

--- migrate/src/java/com/tacitknowledge/util/migration/jdbc/SqlScriptMigrationTaskSource.java
+++ migrate/src/java/com/tacitknowledge/util/migration/jdbc/SqlScriptMigrationTaskSource.java
@@ -14,9 +14,11 @@
 
 package com.tacitknowledge.util.migration.jdbc;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -30,93 +32,224 @@ import com.tacitknowledge.util.migration.MigrationException;
 import com.tacitknowledge.util.migration.MigrationTaskSource;
 
 /**
- * Search a package (directory) for SQL scripts that a specific pattern 
- * and returns corresponding <code>SqlScriptMigrationTasks</code>.  The name
- * of each script must follow the pattern of &quot;patch(\d+)(_.+)?\.sql&quot;.
+ * Search a package (directory) for SQL scripts that a specific pattern and
+ * returns corresponding <code>SqlScriptMigrationTasks</code>. The name of
+ * each script must follow the pattern of &quot;patch(\d+)(_.+)?\.sql&quot;.
  * 
- * @author  Scott Askew (scott@tacitknowledge.com)
+ * @author Scott Askew (scott@tacitknowledge.com)
  */
 public class SqlScriptMigrationTaskSource implements MigrationTaskSource
 {
     /** Class logger */
-    private static Log log = LogFactory.getLog(SqlScriptMigrationTaskSource.class);
-    
+    private static Log log = LogFactory
+	    .getLog(SqlScriptMigrationTaskSource.class);
+
     /**
      * The regular expression used to match SQL patch files.
      */
-    private static final String SQL_PATCH_REGEX = "^patch(\\d+)(_.+)?\\.sql";
+     private static final String SQL_PATCH_REGEX = "^patch(\\d+)_(.+)\\.sql";
+
+    /**
+     * The regular expression used to match SQL rollback files
+     */
+    private static final String SQL_ROLLBACK_REGEX = "^patch(\\d+)-rollback_(.+)\\.sql";
 
     /** {@inheritDoc} */
     public List getMigrationTasks(String packageName) throws MigrationException
     {
-        String path = packageName.replace('.', '/');
-        String[] scripts = ClassDiscoveryUtil.getResources(path, SQL_PATCH_REGEX);
-        if (log.isDebugEnabled())
-        {
-            log.debug("Found " + scripts.length + " patches in path: " + path);
-            for (int i = 0; i < scripts.length; i++)
-            {
-                log.debug(" -- \"" + scripts[i] + "\"");
-            }
-        }
-        return createMigrationScripts(scripts);
+	String path = packageName.replace('.', '/');
+
+	String[] upScripts = getSqlScripts(path,SQL_PATCH_REGEX);
+	String[] downScripts = getSqlScripts(path, SQL_ROLLBACK_REGEX);
+
+	return createMigrationScripts(upScripts, downScripts);
     }
 
     /**
-     * Creates a list of <code>SqlScriptMigrationTask</code>s based on the array
-     * of SQL scripts.
-     *  
-     * @param  scripts the classpath-relative array of SQL migration scripts
-     * @return a list of <code>SqlScriptMigrationTask</code>s based on the array
-     *         of SQL scripts
-     * @throws MigrationException if a SqlScriptMigrationTask could no be created
+     * Returns the SQL scripts which exist in the specified patch and match the specified regex.
+     * @param path the Path to search
+     * @param regex the regex which the scripts should match 
+     * @return a String array of script names.
      */
-    private List createMigrationScripts(String[] scripts) throws MigrationException
+    private String[] getSqlScripts(String path, String regex)
     {
-        Pattern p = Pattern.compile(SQL_PATCH_REGEX);
-        List tasks = new ArrayList();
-        for (int i = 0; i < scripts.length; i++)
-        {
-            String script = scripts[i];
-            script = script.replace('\\', '/');
-            log.debug("Examining possible SQL patch file \"" + script + "\"");
-            InputStream is = 
-                Thread.currentThread().getContextClassLoader().getResourceAsStream(script);
+	String[] scripts = ClassDiscoveryUtil.getResources(path, regex);
+	if (log.isDebugEnabled())
+	{
+	    log.debug("Found " + scripts.length + " patches in path: " + path);
+	    for (int i = 0; i < scripts.length; i++)
+	    {
+		log.debug(" -- \"" + scripts[i] + "\"");
+	    }
+	}
+	return scripts;
+    }
 
-            if (is == null)
-            {
-                log.warn("Could not open input stream for file \"/" + script + "\"");
-            }
-            else
-            {
-                File scriptFile = new File(script);
-                String scriptFileName = scriptFile.getName();
-                try
-                {
-                    // Get the version out of the script name
-                    Matcher matcher = p.matcher(scriptFileName);
-                    if (!matcher.matches() || matcher.groupCount() != 2)
-                    {
-                        throw new MigrationException("Invalid SQL script name: " + script);
-                    }
-                    int order = Integer.parseInt(matcher.group(1));
-                    
-                    // We should send in the script file location so
-                    // it doesn't have to buffer the whole thing into RAM
-                    SqlScriptMigrationTask task
-                        = new SqlScriptMigrationTask(scriptFileName, order, is);
+    /**
+     * Creates a list of <code>SqlScriptMigrationTask</code>s based on the
+     * array of SQL scripts.
+     * 
+     * @param scripts
+     *                the classpath-relative array of SQL migration scripts
+     * @return a list of <code>SqlScriptMigrationTask</code>s based on the
+     *         array of SQL scripts
+     * @throws MigrationException
+     *                 if a SqlScriptMigrationTask could no be created
+     */
+    private List createMigrationScripts(String[] upScripts, String[] downScripts)
+	    throws MigrationException
+    {
+	Pattern upFileNamePattern = Pattern.compile(SQL_PATCH_REGEX);
+	Pattern downFileNamePattern = Pattern.compile(SQL_ROLLBACK_REGEX);
 
-                    // Free the resource
-                    is.close();
-                    task.setName(scriptFileName);
-                    tasks.add(task);
-                }
-                catch (IOException e)
-                {
-                    throw new MigrationException("Error reading script " + script, e);
-                }
-            }
-        }
-        return tasks;
+	List tasks = new ArrayList();
+	for (int i = 0; i < upScripts.length; i++)
+	{
+	    String script = upScripts[i];
+
+	    if (script != null)
+	    {
+		// get the file name
+		File scriptFile = new File(script);
+		String scriptFileName = scriptFile.getName();
+
+		// Get the version out of the script name
+		int order = getOrder(upFileNamePattern, script, scriptFileName);
+
+		// get the down script which matches this patch order
+		String downScript = getMatchingDownScript(downScripts, order,
+			downFileNamePattern);
+
+		// read the scripts
+		String upSql = readSql(getInputStream(script));
+		String downSql = "";
+
+		// if the down script is not the empty string, then try to read
+		// it.
+		if (!"".equals(downScript))
+		    downSql = readSql(getInputStream(downScript));
+
+		// create a new task
+		SqlScriptMigrationTask task = new SqlScriptMigrationTask(
+			scriptFileName, order, upSql, downSql);
+		task.setName(scriptFileName);
+
+		// add the task to the list of tasks
+		tasks.add(task);
+	    }
+	}
+	return tasks;
+    }
+
+    /**
+     * Returns the filename of the downscript
+     * 
+     * @param downScripts
+     * @param order
+     * @return
+     */
+    private String getMatchingDownScript(String[] downScripts, int order,
+	    Pattern fileNamePattern) throws MigrationException
+    {
+	boolean isScriptFound = false;
+	String script = "";
+	for (int i = 0; i < downScripts.length && !isScriptFound; i++)
+	{
+	    File scriptFile = new File(downScripts[i]);
+	    String scriptFileName = scriptFile.getName();
+	    int downScriptOrder = getOrder(fileNamePattern, downScripts[i],
+		    scriptFileName);
+
+	    if (downScriptOrder == order)
+	    {
+		script = downScripts[i];
+		isScriptFound = true;
+	    }
+	}
+
+	if (!isScriptFound)
+	{
+	    log.info("There was no rollback script for patch level: " + order);
+	}
+	return script;
+    }
+
+    /**
+     * Returns an input stream that points to the script name
+     * 
+     * @param scriptName the name of the script to create an InputStream
+     * @return an InputStream returns an InputStream based upon the scriptName
+     */
+    private InputStream getInputStream(String scriptName)
+    {
+	scriptName = scriptName.replace('\\', '/');
+	log.debug("Examining possible SQL patch file \"" + scriptName + "\"");
+	return Thread.currentThread().getContextClassLoader()
+		.getResourceAsStream(scriptName);
+    }
+
+    /**
+     * Reads the file contents into a String object.
+     * 
+     * @param is
+     *                the <code>InputStream</code>
+     * @return a <code>String</code> with the contents of the InputStream
+     * @throws MigrationException
+     *                 if there's an error reading in the contents
+     */
+    private String readSql(InputStream is) throws MigrationException
+    {
+	StringBuffer sqlBuffer = new StringBuffer();
+	BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+	try
+	{
+	    String line = buf.readLine();
+	    while (line != null)
+	    {
+		sqlBuffer.append(line).append("\n");
+		line = buf.readLine();
+	    }
+	} catch (IOException ioe)
+	{
+	    throw new MigrationException(
+		    "There was an error reading in a script", ioe);
+
+	} finally
+	{
+	    try
+	    {
+		is.close();
+	    } catch (IOException ioe)
+	    {
+		log.error("Could not close input stream", ioe);
+	    }
+	}
+	return sqlBuffer.toString();
+    }
+
+    /**
+     * Returns the order for the file.
+     * 
+     * @param p
+     *                a Pattern defining the file name pattern
+     * @param script
+     *                the Script
+     * @param scriptFileName
+     *                the name of the file
+     * @return an int indicating the order
+     * @throws MigrationException
+     *                 in case the file name is invalid
+     */
+    private int getOrder(Pattern p, String script, String scriptFileName)
+	    throws MigrationException
+    {
+	Matcher matcher = p.matcher(scriptFileName);
+	if (!matcher.matches() || matcher.groupCount() != 2)
+	{
+	    throw new MigrationException("Invalid SQL script name: " + script);
+	}
+	int order = Integer.parseInt(matcher.group(1));
+	return order;
     }
 }
