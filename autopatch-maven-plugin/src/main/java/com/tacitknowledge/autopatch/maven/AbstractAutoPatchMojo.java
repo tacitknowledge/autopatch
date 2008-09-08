5a8c7b6ad880432362dac3c05ae1231009dc6d54
woffca <woffca>
2008-09-08 21:12:45 +0000

    Removed the readonly parameter so that it is possible to add custom classpath elements where patches will be located.

--- autopatch-maven-plugin/src/main/java/com/tacitknowledge/autopatch/maven/AbstractAutoPatchMojo.java
+++ autopatch-maven-plugin/src/main/java/com/tacitknowledge/autopatch/maven/AbstractAutoPatchMojo.java
@@ -32,7 +32,6 @@ public abstract class AbstractAutoPatchMojo extends AbstractMojo
     /**
      * @parameter default-value="${project.compileClasspathElements}"
      * @required
-     * @readonly
      */
     protected List classpathElements;
 
