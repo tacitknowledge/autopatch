9ce29030fce170235a36f86687c25038d18693c1
mikehardy <mikehardy>
2009-11-10 22:24:36 +0000

    Greg Roll provided a nice example of integrating AutoPatch with Capistrano

--- website/links.php
+++ website/links.php
@@ -6,6 +6,7 @@
 <h3>Links to related projects and information</h3>
     
 	<ul>
+                <li>Greg Roll has <a href="http://www.codedrop.ca/blog/archives/191">integrated AutoPatch with Capistrano</a> and provided an example. Thanks Greg!
 		<li><a href="http://www.xplanner.org">XPLanner</a> has provided feedback, <a href="http://docs.codehaus.org/display/XPR/Autopatch">documentation</a> and a patch for AutoPatch, thank you XPlanner!</li>
 		<li><a href="http://www.tacitknowledge.com">Tacit Knowledge</a>, they paid for this thing. Thanks!</li>
 		<li><a href="http://www.agiledata.org/essays/databaseRefactoring.html">Database Refactoring</a> - what we're trying to do here.</li>
